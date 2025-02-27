9eb9c04f21002b06b8087f34b2fb23eda4bbff89
brockellis14 <ell19015@byui.edu>
2025-02-27 11:23:21 -0700

    Prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool answer = false;
+        
+        // Generate random number
+        Random newRandomNumber = new Random();
+        int magic = newRandomNumber.Next(1, 100);
+        int x = 0;
+
+        // Start guessing loop
+        while (!answer)
+        {
+            Console.Write("Make a guess: ");
+            int guess = int.Parse(Console.ReadLine());
+            ++x;
+
+            if (guess < magic)
+            {
+                Console.WriteLine("Your guess is too low!");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Your guess is too high!");
+            }
+            else
+            {
+                Console.WriteLine($"Congrats, you guessed correctly, with a total of {x} guesses!");
+                answer = true;
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // List<int> numbers = new List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // numbers.Add(10);
+
+        // foreach(int x in numbers)
+        // {
+        //     Console.WriteLine($"{x}");
+        // }
+
+        // bool correctInput = false;
+        // while(!correctInput)
+        // {
+        //     Console.WriteLine("Please input your age: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age < 120)
+        //     {
+        //         Console.WriteLine($"Your age is: {age}");
+        //         correctInput = true;
+        //     }
+        // }
+        // bool correctInput;
+        // do
+        // {
+        //     Console.WriteLine("Please input your age: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age < 120)
+        //     {
+        //         Console.WriteLine($"Your age is: {age}");
+        //         correctInput = true;
+        //     }
+        //     else
+        //     correctInput = false;
+        // }while(!correctInput);
+        
+        Random newRandomNumber = new Random();
+        for(int i = 1; i< 100; i++)
+        {
+        int number = newRandomNumber.Next(1, 1000);
+        Console.WriteLine($"{i}: {number}");
+        }
     }
 }
