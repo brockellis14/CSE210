4830d28f2f7c59811892fc4f12fbd710bc31fd19
brockellis14 <ell19015@byui.edu>
2025-03-24 21:53:48 -0600

    Created shape parent class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Shape shape1 = new Shape("green");
+        Console.WriteLine(shape1.GetColor());
+        shape1.SetColor("yellow");
+        Console.WriteLine(shape1.GetColor());
+
     }
 }
---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,19 @@
+using System.Formats.Asn1;
+
+class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+}
